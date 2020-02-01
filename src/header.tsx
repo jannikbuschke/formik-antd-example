3a6af62e30dd2f91623160186b0b21a91986b024
Jannik Buschke <jannik.buschke@gmail.com>
2020-02-01 17:56:53 +0100

    fix icons

--- src/header.tsx
+++ src/header.tsx
@@ -1,32 +1,36 @@
-import * as React from "react";
-import { Button } from "antd";
+import * as React from 'react'
+import { Button } from 'antd'
+import { GithubFilled, TwitterOutlined } from '@ant-design/icons'
 
 export const Header = () => (
-  <div className="Header">
-    <h2 style={{color:"#565656"}}>simple declarative bindings</h2>
-    <h1 style={{fontWeight:550,marginTop:0}}>
-      <a href="https://github.com/jaredpalmer/formik" target="_blank">
+  <div className='Header'>
+    <h2 style={{ color: '#565656' }}>simple declarative bindings</h2>
+    <h1 style={{ fontWeight: 550, marginTop: 0 }}>
+      <a href='https://github.com/jaredpalmer/formik' target='_blank'>
         Formik
-      </a>{" "}
-      +{" "}
-      <a href="https://ant.design" target="_blank">
+      </a>{' '}
+      +{' '}
+      <a href='https://ant.design' target='_blank'>
         Ant Design
       </a>
     </h1>
-    <Button.Group size="large">
-      <Button type="primary">
-        <a
-          href="https://www.github.com/jannikbuschke/formik-antd"
-          target="_blank"
-        >
-          {/* Github <Icon type="github" /> */}
-        </a>
+    <Button.Group>
+      <Button
+        href='https://www.github.com/jannikbuschke/formik-antd'
+        target='_blank'
+        style={{ background: '#211F1F', color: 'white' }}
+      >
+        <span>Github </span>
+        <GithubFilled style={{ color: 'white' }} />
       </Button>
-      <Button>
-        <a href="https://www.twitter.com/jannikbuschke" target="_blank">
-          {/* Twitter <Icon type="twitter" /> */}
-        </a>
+      <Button
+        href='https://www.twitter.com/jannikbuschke'
+        target='_blank'
+        style={{ background: '#00acee', color: 'white' }}
+      >
+        <span>Twitter </span>
+        <TwitterOutlined style={{ color: 'white' }} />
       </Button>
     </Button.Group>
   </div>
-);
+)
